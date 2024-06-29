5e674f367c7f3e7522c1a2a2fb30a270cf222443
Bunma93 <bunma93@gmail.com>
2024-06-29 10:24:57 +0700

    Js-condition12

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+let ticket = "";
+while (ticket.length !== 2) {
+    ticket = prompt("กรอกโซนที่นั่ง (ตัวอักษร 2 ตัว)");
+}
+
+if (ticket === "AA") {
+    alert("5,000");
+} else if (ticket === "BB") {
+    alert("4,000");
+} else if (ticket === "CC") {
+    alert("3,000");
+} else if (ticket === "DD") {
+    alert("2,000");
+} else {
+    alert("ไม่มีโซนนี้");
+}
